591622c90267bc55e99198fb885f7f9917ae5c00
Shivam <147581258+shivam7524@users.noreply.github.com>
2025-10-04 14:40:39 +0530

    Delete MSDCollegePaliFinal_15_june_2025/MSDCollegePaliFinal directory

--- MSDCollegePaliFinal_15_june_2025/MSDCollegePaliFinal/MSDCollegePali/MSDCollegePali/obj/Release/net8.0/PubTmp/Out/wwwroot/customjs/Indexjs.js
+++
@@ -1,9 +0,0 @@
-﻿
-    $(document).ready(function () {
-        $('.team-item img').hover(function () {
-            $(this).css({ 'transform': 'rotate(360deg)', 'transition': 'transform 0.6s ease-in-out' });
-        }, function () {
-            $(this).css({ 'transform': 'rotate(0deg)', 'transition': 'transform 0.6s ease-in-out' });
-        });
-    });
-
